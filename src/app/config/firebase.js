b391927fdba710c472f3ed57a17cf0e3d9e2aebc
matko-antunovic <47244433+matko-antunovic@users.noreply.github.com>
2019-04-17 16:14:00 +0200

    upd

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -4,12 +4,12 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const firebaseConfig= {
-    apiKey: "AIzaSyDs9vNUaB2_cmRkyrlMoBp5r51KR_HRt-Q",
+    apiKey: "*********",
     authDomain: "re-events-236319.firebaseapp.com",
     databaseURL: "https://re-events-236319.firebaseio.com",
-    projectId: "re-events-236319",
-    storageBucket: "re-events-236319.appspot.com",
-    messagingSenderId: "162909981432"
+    projectId: "r*****",
+    storageBucket: "re-eve******.com",
+    messagingSenderId: "162***81432"
 }
 
 firebase.initializeApp(firebaseConfig);
